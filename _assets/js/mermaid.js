f2bead5e094f49568ef29ef48f38e3c9882431a5
israel-assis <israel.dynamics@gmail.com>
2022-11-29 12:15:59 -0300

    Deployed d9eaacf with MkDocs version: 1.4.1

---
+++ _assets/js/mermaid.js
@@ -0,0 +1,10 @@
+mermaidAPI.initialize({
+  theme: "default",
+  themeVariables: {
+    nodeBorder: "#004990",
+    mainBkg: "#c9d7e4",
+    nodeTextColor: "#274059",
+    fontFamily: "arial",
+    fontSize: "18px",
+  },
+});
